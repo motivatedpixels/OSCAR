3fd26b73b5d5df794d50ef7ea4e271bfcf900b4c
Matt Rhodes <matt.rhodes@autodesk.com>
2026-01-27 20:34:24 -0800

    Updated icon and About box.

--- build.sh
+++ build.sh
@@ -8,11 +8,11 @@ xcodebuild -project OSCAR.xcodeproj -scheme OSCAR -configuration Debug build
 if [ $? -eq 0 ]; then
     echo "Build succeeded. Copying app to bin folder..."
     mkdir -p bin
-    rm -rf bin/OSCAR.app
-    cp -R ~/Library/Developer/Xcode/DerivedData/OSCAR-*/Build/Products/Debug/OSCAR.app bin/
+    rm -rf bin/O.S.C.A.R..app
+    cp -R ~/Library/Developer/Xcode/DerivedData/OSCAR-*/Build/Products/Debug/O.S.C.A.R..app bin/
 
-    echo "Done! App is at: bin/OSCAR.app"
-    echo "Run with: open bin/OSCAR.app"
+    echo "Done! App is at: bin/O.S.C.A.R..app"
+    echo "Run with: open bin/O.S.C.A.R..app"
 else
     echo "Build failed."
     exit 1
